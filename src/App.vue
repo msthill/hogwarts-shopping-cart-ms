98fec4ec9ff4ed19846bdb5c0ba0c95f085e6136
msthill <micah.sthill123@gmail.com>
2026-03-01 15:39:31 -0600

    v-on

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,9 @@
                         </div>
                         <div class="item-actions">
                             <div class="quantity-selector">
-                                <button class="quantity-change-button">−</button>
-                                <input type="text" class="quantity-input" :value="shoppingCartItems[0].quantity" aria-label="quantity">
-                                <button class="quantity-change-button">+</button>
+                                <button class="quantity-change-button" @click="decreaseOne(shoppingCartItems[0].id)">−</button>
+                                <input type="text" class="quantity-input" :value="shoppingCartItems[0].quantity" aria-label="quantity" @blur="changeQuantity(shoppingCartItems[0].id, $event)">
+                                <button class="quantity-change-button" @click="increaseOne(shoppingCartItems[0].id)">+</button>
                             </div>
                             <button class="remove-item">✕</button>
                         </div>
@@ -169,6 +169,32 @@ let shoppingCartItems = ref([
 ])
 
 let hideDetails = ref(false);
+
+function decreaseOne(id) {
+  shoppingCartItems.value.some((item => {
+    if (item.id === id && item.quantity != 0) {
+      item.quantity = item.quantity - 1;
+    }
+  }))
+
+}
+
+function increaseOne(id) {
+  shoppingCartItems.value.some((item => {
+    if (item.id === id) {
+      item.quantity = item.quantity + 1;
+    }
+  }))
+}
+
+function changeQuantity(id, event) {
+  shoppingCartItems.value.some((item => {
+    if (item.id === id) {
+      item.quantity = parseInt(event.target.value);
+    }
+  }))
+
+}
 </script>
 
 <style scoped>
